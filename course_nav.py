3fecde94a1e9302e5370ee49a9652b93ea3c97fc
RyanTDL <teodeliangryan@gmail.com>
2023-03-16 21:04:54 +0800

    Code works: Can now deliver to table 1 and return to dock

--- course_nav.py
+++ course_nav.py
@@ -1,251 +1,273 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import rclpy
 from rclpy.node import Node
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
 from rclpy.qos import qos_profile_sensor_data
 from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import OccupancyGrid
+import numpy as np
+import math
+import cmath
+import time
 
+# constants
+rotatechange = 0.1
+speedchange = 0.05
+occ_bins = [-1, 0, 100, 101]
+stop_distance = 0.5
+front_angle = 1
+front_angles = range(-front_angle,front_angle+1,1)
+scanfile = 'lidar.txt'
+mapfile = 'map.txt'
+
+# code from https://automaticaddison.com/how-to-convert-a-quaternion-into-euler-angles-in-python/
+def euler_from_quaternion(x, y, z, w):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = math.atan2(t0, t1)
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch_y = math.asin(t2)
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = math.atan2(t3, t4)
+
+    return roll_x, pitch_y, yaw_z # in radians
 
 class AutoNav(Node):
 
     def __init__(self):
         super().__init__('auto_nav')
-
-        #Create subscriber to track odometry, aka position
+        
+        # create publisher for moving TurtleBot
+        self.publisher_ = self.create_publisher(Twist,'cmd_vel',10)
+        # self.get_logger().info('Created publisher')
+        
+        # create subscription to track orientation
         self.odom_subscription = self.create_subscription(
             Odometry,
             'odom',
-            self.odom_callback, 
+            self.odom_callback,
+            10)
+        # self.get_logger().info('Created subscriber')
+        self.odom_subscription  # prevent unused variable warning
+        # initialize variables
+        self.roll = 0
+        self.pitch = 0
+        self.yaw = 0
+        
+        # create subscription to track occupancy
+        self.occ_subscription = self.create_subscription(
+            OccupancyGrid,
+            'map',
+            self.occ_callback,
             qos_profile_sensor_data)
-        self.odom_subscription #prevent unused variable
-
-        #Create subscriber to track Lidar
-        self.scan_subscription= self.create_subscription(
+        self.occ_subscription  # prevent unused variable warning
+        self.occdata = np.array([])
+        
+        # create subscription to track lidar
+        self.scan_subscription = self.create_subscription(
             LaserScan,
             'scan',
             self.scan_callback,
             qos_profile_sensor_data)
-        self.scan_subscription #prevent unused variable warning
+        self.scan_subscription  # prevent unused variable warning
+        self.laser_range = np.array([])
+        self.lri= 5
+
 
 
     def odom_callback(self, msg):
-        self.odomtest=1.5
+        # self.get_logger().info('In odom_callback')
+        orientation_quat =  msg.pose.pose.orientation
+        self.roll, self.pitch, self.yaw = euler_from_quaternion(orientation_quat.x, orientation_quat.y, orientation_quat.z, orientation_quat.w)
+
+
+    def occ_callback(self, msg):
+        # self.get_logger().info('In occ_callback')
+        # create numpy array
+        msgdata = np.array(msg.data)
+        # compute histogram to identify percent of bins with -1
+        # occ_counts = np.histogram(msgdata,occ_bins)
+        # calculate total number of bins
+        # total_bins = msg.info.width * msg.info.height
+        # log the info
+        # self.get_logger().info('Unmapped: %i Unoccupied: %i Occupied: %i Total: %i' % (occ_counts[0][0], occ_counts[0][1], occ_counts[0][2], total_bins))
+
+        # make msgdata go from 0 instead of -1, reshape into 2D
+        oc2 = msgdata + 1
+        # reshape to 2D array using column order
+        # self.occdata = np.uint8(oc2.reshape(msg.info.height,msg.info.width,order='F'))
+        self.occdata = np.uint8(oc2.reshape(msg.info.height,msg.info.width))
+        # print to file
+        np.savetxt(mapfile, self.occdata)
+
 
     def scan_callback(self, msg):
-        self.scantest=2
+        # self.get_logger().info('In scan_callback')
+        # create numpy array
+        self.laser_range = np.array(msg.ranges)
+        # print to file
+        np.savetxt(scanfile, self.laser_range)
+        # replace 0's with nan
+        self.laser_range[self.laser_range==0] = np.nan
 
 
-    def move(self):
-        try:
+
+    # function to rotate the TurtleBot
+    def rotatebot(self, rot_angle):
+        # self.get_logger().info('In rotatebot')
+        # create Twist object
+        twist = Twist()
+        
+        # get current yaw angle
+        current_yaw = self.yaw
+        # log the info
+        self.get_logger().info('Current: %f' % math.degrees(current_yaw))
+        # we are going to use complex numbers to avoid problems when the angles go from
+        # 360 to 0, or from -180 to 180
+        c_yaw = complex(math.cos(current_yaw),math.sin(current_yaw))
+        # calculate desired yaw
+        target_yaw = current_yaw + math.radians(rot_angle)
+        # convert to complex notation
+        c_target_yaw = complex(math.cos(target_yaw),math.sin(target_yaw))
+        self.get_logger().info('Desired: %f' % math.degrees(cmath.phase(c_target_yaw)))
+        # divide the two complex numbers to get the change in direction
+        c_change = c_target_yaw / c_yaw
+        # get the sign of the imaginary component to figure out which way we have to turn
+        c_change_dir = np.sign(c_change.imag)
+        # set linear speed to zero so the TurtleBot rotates on the spot
+        twist.linear.x = 0.0
+        # set the direction to rotate
+        twist.angular.z = c_change_dir * rotatechange
+        # start rotation
+        self.publisher_.publish(twist)
+
+        # we will use the c_dir_diff variable to see if we can stop rotating
+        c_dir_diff = c_change_dir
+        # self.get_logger().info('c_change_dir: %f c_dir_diff: %f' % (c_change_dir, c_dir_diff))
+        # if the rotation direction was 1.0, then we will want to stop when the c_dir_diff
+        # becomes -1.0, and vice versa
+        while(c_change_dir * c_dir_diff > 0):
+            # allow the callback functions to run
             rclpy.spin_once(self)
+            current_yaw = self.yaw
+            # convert the current yaw to complex form
+            c_yaw = complex(math.cos(current_yaw),math.sin(current_yaw))
+            # self.get_logger().info('Current Yaw: %f' % math.degrees(current_yaw))
+            # get difference in angle between current and target
+            c_change = c_target_yaw / c_yaw
+            # get the sign to see if we can stop
+            c_dir_diff = np.sign(c_change.imag)
+            # self.get_logger().info('c_change_dir: %f c_dir_diff: %f' % (c_change_dir, c_dir_diff))
+
+        self.get_logger().info('End Yaw: %f' % math.degrees(current_yaw))
+        # set the rotation speed to 0
+        twist.angular.z = 0.0
+        # stop the rotation
+        self.publisher_.publish(twist)
+
+
+    def stopbot(self):
+        # publish to cmd_vel to move TurtleBot
+        twist = Twist()
+        twist.linear.x = 0.0
+        twist.angular.z = 0.0
+        # time.sleep(1)
+        self.publisher_.publish(twist)
+        self.get_logger().info('Robot has reached destination')
+
+    def forward(self):
+        # start moving
+        twist = Twist()
+        twist.linear.x = speedchange
+        twist.angular.z = 0.0
+        # not sure if this is really necessary, but things seem to work more
+        # reliably with this
+        time.sleep(1)
+        self.publisher_.publish(twist)
+
+
+    def move_front(self):
+        if self.laser_range.size != 0:
+            # # check distances in front of TurtleBot and find values less than stop_distance
+            # lri = (self.laser_range[front_angles]<float(stop_distance)).nonzero()
+            front_dist= self.laser_range[front_angle]
+        
+            while front_dist>stop_distance:
+                # allow the callback functions to run
+                rclpy.spin_once(self)
+                front_dist= self.laser_range[front_angle]
+                print('Front distance: '+str(front_dist))
+                self.forward()
+            
+            self.stopbot()  
+
+
+    def table1(self):
+        if self.laser_range.size != 0:  #This line to ensure move_front will run first, before rotatebot as scan message has delay to be received
+            self.move_front()
+            self.rotatebot(180)
+            self.move_front()
+            rclpy.shutdown()
+
+        
+    def mover(self):
+        try:
+            self.stopbot()
+            #self.forward()
 
             while rclpy.ok():
-                print(self.scantest)
+                self.table1()
+
+                
+                # self.get_logger().info('Start moving')
+                # self.table1()
+                
+                    
+                # allow the callback functions to run
+                rclpy.spin_once(self)
+
         except Exception as e:
             print(e)
-
+        
+        # Ctrl-c detected
+        finally:
+            # stop moving
+            self.stopbot()
 
 
 def main(args=None):
     rclpy.init(args=args)
-    auto_nav= AutoNav()
-    auto_nav.move()
+
+    auto_nav = AutoNav()
+    auto_nav.mover()
     auto_nav.destroy_node()
     rclpy.shutdown()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
-
-
-
-
-# import rclpy
-# from rclpy.node import Node
-# from rclpy.qos import qos_profile_sensor_data
-# from sensor_msgs.msg import LaserScan
-# from nav_msgs.msg import Odometry
-# from geometry_msgs.msg import Twist
-# import numpy as np
-# import math
-# import cmath
-
-
-# # code from https://automaticaddison.com/how-to-convert-a-quaternion-into-euler-angles-in-python/
-# def euler_from_quaternion(x, y, z, w):
-#     """
-#     Convert a quaternion into euler angles (roll, pitch, yaw)
-#     roll is rotation around x in radians (counterclockwise)
-#     pitch is rotation around y in radians (counterclockwise)
-#     yaw is rotation around z in radians (counterclockwise)
-#     """
-#     t0 = +2.0 * (w * x + y * z)
-#     t1 = +1.0 - 2.0 * (x * x + y * y)
-#     roll_x = math.atan2(t0, t1)
-
-#     t2 = +2.0 * (w * y - z * x)
-#     t2 = +1.0 if t2 > +1.0 else t2
-#     t2 = -1.0 if t2 < -1.0 else t2
-#     pitch_y = math.asin(t2)
-
-#     t3 = +2.0 * (w * z + x * y)
-#     t4 = +1.0 - 2.0 * (y * y + z * z)
-#     yaw_z = math.atan2(t3, t4)
-
-#     return roll_x, pitch_y, yaw_z  # in radians
-
-
-
-
-
-# class AutoNav(Node):
-
-#     def __init__(self):
-#         super().__init__('auto_nav')
-
-#         #Create publisher for moving turtlebot
-#         self.vel_publisher= self.create_publisher(Twist,'cmd_vel',10)
-
-#         #Create subscriber to track odometry, aka position
-#         self.odom_subscription = self.create_subscription(
-#             Odometry,
-#             'odom',
-#             self.odom_callback, 
-#             qos_profile_sensor_data)
-#         #self.odom_subscription #prevent unused variable
-
-#         #Create subscriber to track Lidar
-#         self.scan_subscription= self.create_subscription(
-#             LaserScan,
-#             'scan',
-#             self.scan_callback,
-#             qos_profile_sensor_data)
-#         #self.scan_subscription #prevent unused variable warning
-
-
-
-
-#     def odom_callback(self, msg):
-#         self.odomtest=1.5
-#         # orientation_quat= msg.pose.pose.orientation
-#         # position= msg.pose.pose.position
-#         # #Yaw goes from 0->-3.14->3,14->0
-#         # self.roll, self.pitch, self.yaw= euler_from_quaternion(orientation_quat.x,orientation_quat.y,orientation_quat.z,orientation_quat.w)
-#         # self.yaw+=math.pi #convert from '-180 to 180', to become '0 to 360'
-#         # self.locx, self.locy= position.x, position.y
-
-
-#     def scan_callback(self, msg):
-#         self.scantest=2
-#         # laser_range= np.array(msg.ranges[:100])
-#         # laser_range[laser_range==0] =np.nan #Replace NaN values with 0
-#         # self.front= laser_range[0] #Takes front of Lidar, aka 0 degree  
-#         # #print(laser_range[0], laser_range[90], laser_range[180], laser_range[270])
-#         # #self.get_logger().info('Front distance is %s' % self.front)
-
-
-    
-#     def rotate_left(self):
-#         twist= Twist() #create Twist object
-#         current_yaw= self.yaw #get current yaw
-#         target_yaw = current_yaw + math.radians(90) #calculate desired yaw
-#         if target_yaw>2*math.pi:
-#             target_yaw -= 2*math.pi
-
-#         while not target_yaw-0.02<=self.yaw<=target_yaw+0.02:
-#             rclpy.spin_once(self) #allows callbacks to be called once
-#             twist.linear.x= 0.0 
-#             twist.angular.z= 0.3
-#             self.vel_publisher.publish(twist)
-#             #print(self.yaw, target_yaw)
-
-#         twist.angular.z= 0.0 #stops rotation
-#         self.vel_publisher.publish(twist)
-
-
-#     def rotate_right(self):
-#         twist= Twist() #create Twist object
-#         current_yaw= self.yaw #get current yaw
-#         target_yaw = current_yaw - math.radians(90) #calculate desired yaw
-#         if target_yaw>2*math.pi:
-#             target_yaw -= 2*math.pi
-
-#         while not target_yaw-0.01<=self.yaw<=target_yaw+0.01:
-#             rclpy.spin_once(self) #allows callbacks to be called once
-#             twist.linear.x= 0.0 
-#             twist.angular.z= -0.3
-#             self.vel_publisher.publish(twist)
-#             #print(self.yaw, target_yaw)
-
-#         twist.angular.z= 0.0 #stops rotation
-#         self.vel_publisher.publish(twist)
-
-
-#     def rotate_back(self):
-#         twist= Twist() #create Twist object
-#         current_yaw= self.yaw #get current yaw
-#         target_yaw = current_yaw + math.radians(180) #calculate desired yaw
-#         if target_yaw>2*math.pi:
-#             target_yaw -= 2*math.pi
-
-#         while not target_yaw-0.01<=self.yaw<=target_yaw+0.01:
-#             rclpy.spin_once(self) #allows callbacks to be called once
-#             twist.linear.x= 0.0 
-#             twist.angular.z= 0.3
-#             self.vel_publisher.publish(twist)
-#             #print(self.yaw, target_yaw)
-
-#         twist.angular.z= 0.0 #stops rotation
-#         self.vel_publisher.publish(twist)
-#         rclpy.shutdown()
-
-    
-#     def forward(self, distance):
-#         pass
-#         # twist= Twist()
-#         # while self.front>distance:
-#         #     twist.linear.x=0.2
-#         #     self.vel_publisher.publish(twist)
-
-#         # twist.linear.x=0.0
-#         # self.vel_publisher.publish(twist)
-
-#     def backward(self):
-#         twist= Twist()
-#         twist.linear.x=-0.2
-#         self.vel_publisher.publish(twist)
-
-#     def table_1(self):
-#         self.forward(0.1)
-        
-#         # served=input('Can received?: True/False')
-#         # if served=='True':
-#         #     self.rotate_back()
-#         #     self.forward(0.1)
-
-
-#     def move(self):
-#         try:
-#             rclpy.spin_once(self)
-
-#             while rclpy.ok():
-#                 #print(self.callback(self.odom_sub, self.scan_sub))
-#                 print(self.odomtest)
-#                 #self.callback()
-#         except Exception as e:
-#             print(e)
-
-
-        
-
-
-# def main(args=None):
-#     rclpy.init(args=args)
-#     auto_nav= AutoNav()
-#     auto_nav.move()
-#     auto_nav.destroy_node()
-#     rclpy.shutdown()
-
-
-# if __name__=='__main__':
-#     main()
